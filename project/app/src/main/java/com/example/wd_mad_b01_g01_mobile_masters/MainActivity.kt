4f928d2c0f96c430970724612abab171a01df486
Harini Senevirathne <99156018+IT21386022@users.noreply.github.com>
2023-05-03 12:13:43 +0530

    Add files via upload

---
+++ project/app/src/main/java/com/example/wd_mad_b01_g01_mobile_masters/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wd_mad_b01_g01_mobile_masters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
